d8e284d12def1424d3e936e30d86d4d8b9a0f085
Hamza Koc <hamzakooc@gmail.com>
2022-12-10 00:26:05 -0500

    IaC created

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+# Resource Group, VM, Storage Account  with Terraform
+
+
+Before Deployment
+![Alt text](beforeDeploy.png?raw=true "beforeDeploy")
+
+
+After Deployment
+![Alt text](deployment.png?raw=true "deployment")
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "location" {
+  default = "canadacentral"
+}
+
+variable "resource_group_name" {
+  default = "RG1"
+}
+
+variable "storage_account_name" {
+  default = "storagehamzatest1"
+}
+
+variable "vm_name" {
+  default = "hamzavmtest"
+}
+
+variable "vm_user_name" {
+  default = "hamzavmtest"
+}
+
+variable "vm_user_password" {
+  default = "Hamzavmtest123"
+}
